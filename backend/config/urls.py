46635a8451bc27202f6fd25da18eb079a5496648
Devivaraprasad Baratam <devivaraprasadbaratam@Devivaraprasads-MacBook-Air.local>
2026-05-26 23:33:37 +0530

    Added homepage route

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
-
+def home(request):
+    return HttpResponse("Backend is Live 🚀")
 urlpatterns = [
 
     path(
